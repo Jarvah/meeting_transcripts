1ebb99790e696b6562a17c5c237c14783b807d86
SG0014000052M <sg0014000052m@SG0014000052Ms-MacBook-Air.local>
2020-03-29 17:13:56 +0800

    update database url

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 import subprocess
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 MEETING_ID_PATTERN = '^meet-[a-z]{5}$'
 TIMESTAMP_PATTERN = '\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z'
 TRANSCRIPTIONS_TABLE_COLUMNS = ['meeting_id', 'speaker_name', 'message', 'spoken_at']
 
 class Config():
-    proc = subprocess.Popen('heroku config:get DATABASE_URL -a meeting-transcript', stdout=subprocess.PIPE, shell=True)
-    DATABASE_URL = proc.stdout.read().decode('utf-8').strip() + '?sslmode=require'
-    print(DATABASE_URL)
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    # proc = subprocess.Popen('heroku config:get DATABASE_URL -a meeting-transcript', stdout=subprocess.PIPE, shell=True)
+    # DATABASE_URL = proc.stdout.read().decode('utf-8').strip() + '?sslmode=require'
+    # print(DATABASE_URL)
+    SQLALCHEMY_DATABASE_URI = os.environ.get['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
